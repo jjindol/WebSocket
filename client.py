da6c54e3329b682c8d2181b4414d96f1e9d6c660
jjindol <andy15948@naver.com>
2023-11-27 18:34:30 +0900

    최종 code

--- client.py
+++ client.py
@@ -1,10 +1,32 @@
 import socket
+import tkinter as tk
+from tkinter import simpledialog
 
-HOST = "127.0.0.1"
-PORT = 12346
+def send_data_to_server():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('192.168.0.7', 8011))
+    
+    while True:
+        # Get user input from GUI
+        user_input = simpledialog.askstring("Input", "계속하실건가요? (yes/no)",
+                                    parent=application_window)
+        if user_input is None: # If the dialog is canceled, return
+            break
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall(b"")
-data = s.recv(1024)
-print("Response: {}".format(data))
+        client.send(user_input.encode('utf-8'))
+        response = client.recv(1024).decode('utf-8')
+        text_widget.insert(tk.END, response)
+
+# Create the main window
+application_window = tk.Tk()
+
+# Create a text widget
+text_widget = tk.Text(application_window)
+text_widget.pack()
+
+# Create a button that will call the send_data_to_server function
+button = tk.Button(application_window, text="Send", command=send_data_to_server)
+button.pack()
+
+# Run the Tkinter event loop
+application_window.mainloop()
